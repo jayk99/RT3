0cdcfa901517ecbb9493c1442b19cc74b6a968f1
Jaya krishna <Krishna@Krishnas-MacBook-Air.local>
2023-10-30 23:11:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ColorPicker from './ColorPicker';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-title">Color Picker</h1>
+      <div className="content">
+        <ColorPicker />
+      </div>
+    </div>
+  );
+}
+
+export default App;
